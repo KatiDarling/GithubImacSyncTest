8c03e1b138ebd13b45a13831b3c6c33ec1dfa892
KatiDarling <kati.bicknell@gmail.com>
2024-03-01 14:38:50 -0700

    Initial Commit

---
+++ GithubImacSyncTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubImacSyncTest
+//
+//  Created by Kati Bicknell on 3/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubImacSyncTest/GithubImacSyncTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubImacSyncTestApp.swift
+//  GithubImacSyncTest
+//
+//  Created by Kati Bicknell on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubImacSyncTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
